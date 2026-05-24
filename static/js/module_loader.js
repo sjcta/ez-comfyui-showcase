ad8cf44da78a6e635b8a47397857ffa67ec61efb
Jeson Sun <sjcta@users.noreply.github.com>
2026-05-25 01:55:05 +0800

    Unify mobile composer button motion

--- static/js/module_loader.js
+++ static/js/module_loader.js
@@ -7,7 +7,7 @@
   'use strict';
 
   var base = 'static/js';
-  var version = '1779645084';
+  var version = '1779645219';
   var runtimeApiBase = (location.protocol === 'file:')
     ? 'http://localhost:18000'
     : '';
